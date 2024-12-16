c718d97c2e5138f6f3b37ac0df51e63b4baef8c9
Isidora Vidojevic <isidora.vid@gmail.com>
2024-12-16 16:33:28 +0100

    Basepath fix

--- common/pathHelper.ts
+++ common/pathHelper.ts
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 export function getMediaPathname(image: string) {
-    return isProd ? `/test/${image}` : `/${image}`;
+    return isProd ? `/testmastersolutions/${image}` : `/${image}`;
 }
