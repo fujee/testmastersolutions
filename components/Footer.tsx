34d62c8c943cdbaf71a15a62d5f3daecb8816998
Isidora Vidojevic <isidora.vid@gmail.com>
2025-01-15 10:36:01 +0100

    Izmenjen logo u footer-u

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,18 +22,6 @@ export default function Footer() {
                             height={40}
                         />
                         <p className="text-xs lg:text-sm text-[#888] my-2 text-center">{t("Наш тим покрива све – од идеје до реализације", "footer_team")}</p>
-                        <a href="http://www.fon.bg.ac.rs/" target="_blank">
-                            <Image
-                                className="p-3"
-                                src={getMediaPathname("fon-logo-light.png")}
-                                alt="FON"
-                                width={130}
-                                height={20}
-                            />
-                        </a>
-                        <p className="text-xs lg:text-sm text-[#888] text-center leading-relaxed">{t("Подржано од стране", "footer_supported")} <br />
-                            <a href="http://www.fon.bg.ac.rs/" target="_blank" className="hover:text-primary transition-colors duration-100 text-center">{t("Факултета организационих наука", "footer_fon")}</a>
-                        </p>
                     </div>
                     <div className="mt-6 ml-4 lg:ml-0 lg:mt-0 lg:col-span-1 text-xs lg:text-sm">
                         <h6 className="text-[#a68968] font-semibold uppercase tracking-widest text-xs mb-6">Партнери</h6>
@@ -61,6 +49,16 @@ export default function Footer() {
                             <h6 className="text-[#a68968] font-semibold uppercase tracking-widest text-xs mb-2">{t("Електронска пошта", "footer_email")}</h6>
                         </div>
                         <p className="text-[#888] hover:text-primary transition-colors duration-100 text-sm"><a href="mailto:contact@testmastersolutions.com">contact@testmastersolutions.com</a></p>
+                        <p className="text-sm mt-8 text-[#888] leading-relaxed">{t("Подржано од стране", "footer_supported")}</p>
+                        <a href="http://www.fon.bg.ac.rs/" target="_blank">
+                            <Image
+                                className="p-3"
+                                src={getMediaPathname("fon-logo.png")}
+                                alt="FON"
+                                width={280}
+                                height={100}
+                            />
+                        </a>
                     </div>
                 </div>
             </div>
