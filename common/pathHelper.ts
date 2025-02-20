33506b4b5c2078e8f413dfa2ad6cddce75b30acb
Isidora Vidojevic <isidora.vid@gmail.com>
2025-02-20 12:35:31 +0100

    Izmena

--- common/pathHelper.ts
+++ common/pathHelper.ts
@@ -1,7 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 export function getMediaPathname(image: string) {
-    return isProd ? `/testmastersolutions/${image}` : `/${image}`;
+    return `/${image}`;
 }
 
 export function isValidPath(path: string) {
